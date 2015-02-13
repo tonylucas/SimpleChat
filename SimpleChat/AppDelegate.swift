adc5a26fa70ddf62b073f82034d99c853145e317
tonylucas <tony3lucas@hotmail.fr>
2015-02-13 16:34:32 +0100

    Initial commit

---
+++ SimpleChat/AppDelegate.swift
@@ -0,0 +1,110 @@
+//
+//  AppDelegate.swift
+//  SimpleChat
+//
+//  Created by Tony Lucas on 12/02/2015.
+//  Copyright (c) 2015 Tony Lucas. All rights reserved.
+//
+
+import UIKit
+import MultipeerConnectivity
+
+let serviceType = "simple-chat2"
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate, MCSessionDelegate, MCAdvertiserAssistantDelegate {
+
+    var window: UIWindow?
+    var session: MCSession?
+    var notificationCenter = NSNotificationCenter()
+    var advertiserAssistant: MCAdvertiserAssistant?
+    var users = NSMutableArray()
+    
+    var dict = NSMutableDictionary()
+    
+    class func appDelegate() -> AppDelegate {
+        return UIApplication.sharedApplication().delegate as AppDelegate
+    }
+    
+    class func sharedSession() -> MCSession? {
+        return self.appDelegate().session
+    }
+
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        // Override point for customization after application launch.
+        
+        let peerID = MCPeerID(displayName: "Tony")
+        session = MCSession(peer: peerID, securityIdentity: nil, encryptionPreference: .Required)
+        session?.delegate = self
+
+        dispatch_async(dispatch_get_main_queue()) {
+            self.advertiserAssistant = MCAdvertiserAssistant(
+                serviceType: serviceType,
+                discoveryInfo: nil,
+                session: self.session!)
+            self.advertiserAssistant!.start()
+            println("Start advertising")
+        }
+        
+        return true
+    }
+    
+
+
+    func applicationWillResignActive(application: UIApplication) {
+        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    }
+
+    func applicationDidEnterBackground(application: UIApplication) {
+        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    }
+
+    func applicationWillEnterForeground(application: UIApplication) {
+        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+    }
+
+    func applicationDidBecomeActive(application: UIApplication) {
+        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    }
+
+    func applicationWillTerminate(application: UIApplication) {
+        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+
+    func session(session: MCSession!, didReceiveData data: NSData!, fromPeer peerID: MCPeerID!) {
+        println("didReceiveData")
+        let data = UIImage(data: data)
+        dict.setObject(data!, forKey: peerID)
+        
+        notificationCenter.postNotificationName(newUserNotification, object: nil, userInfo: nil)
+    }
+    
+    
+    
+    func session(session: MCSession!, didReceiveStream stream: NSInputStream!, withName streamName: String!, fromPeer peerID: MCPeerID!) {
+        println("didReceiveStream")
+    }
+    
+    func session(session: MCSession!, didStartReceivingResourceWithName resourceName: String!, fromPeer peerID: MCPeerID!, withProgress progress: NSProgress!) {
+        println("didStartReceivingResourceWithName")
+    }
+    
+    
+    func session(session: MCSession!, didFinishReceivingResourceWithName resourceName: String!, fromPeer peerID: MCPeerID!, atURL localURL: NSURL!, withError error: NSError!) {
+        println("didFinishReceivingResourceWithName")
+    }
+    
+    
+    func session(session: MCSession!, peer peerID: MCPeerID!, didChangeState state: MCSessionState) {
+        println("peerID: \(peerID) didChangeState: \(state.rawValue)")
+        
+        if state == .Connected {
+            users.insertObject(peerID, atIndex: 0)
+            
+        }
+    }
+}
+
---
+++ SimpleChat/ViewController.swift
@@ -0,0 +1,141 @@
+//
+//  ViewController.swift
+//  SimpleChat
+//
+//  Created by Tony Lucas on 13/02/2015.
+//  Copyright (c) 2015 Tony Lucas. All rights reserved.
+//
+
+import UIKit
+import MultipeerConnectivity
+
+let newUserNotification = "AddNewUserNotification"
+
+class ViewController: UITableViewController, MCBrowserViewControllerDelegate {
+    
+    let appDelegate = AppDelegate.appDelegate()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        let notificationCenter = NSNotificationCenter.defaultCenter()
+        notificationCenter.addObserver(self, selector: "userInsert:", name: newUserNotification, object: nil)
+    }
+    
+    
+    func userInsert(notification: NSNotification){
+        println("userInsert")
+        let indexPath = NSIndexPath(forRow: 0, inSection: 0)
+        self.tableView.insertRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
+    }
+    
+    
+    @IBAction func inviteAction(sender: UIButton) {
+        
+        if let session = AppDelegate.sharedSession(){
+            let browserViewController = MCBrowserViewController(serviceType: serviceType, session: appDelegate.session)
+            browserViewController.delegate = self
+            
+            presentViewController(browserViewController, animated: true, completion: nil)
+        }
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func browserViewControllerDidFinish(browserViewController: MCBrowserViewController!){
+        println("browserViewControllerDidFinish")
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func browserViewControllerWasCancelled(browserViewController: MCBrowserViewController!) {
+        println("browserViewControllerWasCancelled")
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    // MARK: - Table view data source
+    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Potentially incomplete method implementation.
+        // Return the number of sections.
+        return 1
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete method implementation.
+        // Return the number of rows in the section.
+        return appDelegate.users.count
+    }
+    
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let peerID = appDelegate.users[indexPath.row] as MCPeerID
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as UITableViewCell
+        
+        
+//        let peerID = dict["peerID"] as MCPeerID
+        
+        let name = peerID.displayName
+        cell.textLabel!.text = name
+        
+        if !(dict[peerID] is NSNull) {
+            let image = dict[peerID] as UImage
+        }
+        
+        
+        return cell
+    }
+    
+    
+    
+    // Override to support conditional editing of the table view.
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return NO if you do not want the specified item to be editable.
+        return true
+    }
+    
+    
+    
+    // Override to support editing the table view.
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            // Delete the row from the data source
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        } else if editingStyle == .Insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }
+    }
+    
+    
+    
+    // Override to support rearranging the table view.
+    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+        
+    }
+    
+    
+    
+    // Override to support conditional rearranging of the table view.
+    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return NO if you do not want the item to be re-orderable.
+        return true
+    }
+    
+    
+    
+    // MARK: - Navigation
+    
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    
+    
+    
+}
